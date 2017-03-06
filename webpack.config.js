470a0524c5e951d2e6056eb31acc9b488419b136
Taylor Lovett <tlovett88@gmail.com>
2017-03-06 01:10:02 -0500

    Proper loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var loaders = [
-  {
-    test: /\.jsx?$/,
-    exclude: /(node_modules|bower_components)/,
-    loader: 'babel-loader'
-  },
-  {
-    test: /\.scss$/,
-    loaders: ['style-loader', 'css-loader', 'sass-loader']
-  }
-];
-
 module.exports = {
   entry: __dirname + '/src/index.js',
   devtool: 'inline-source-map',
   watch: true,
   output: {
-      path:'/',
-      filename: 'dist/client.js'
+    path:'/',
+    filename: 'dist/client.js'
   },
   module: {
-    loaders: loaders
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              includePaths: [path.join(__dirname, 'node_modules/bootstrap-sass/assets/stylesheets')]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
   },
   stats: {
     colors: true
+  },
+  resolveLoader: {
+    modules: [path.join(__dirname, 'node_modules')]
+  },
+  resolve: {
+    modules: [path.join(__dirname, 'node_modules')]
   },
   plugins: [
     new webpack.DefinePlugin({
